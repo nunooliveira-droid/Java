5698ee44e48d82d61bcaf9a1e8227da9c3eab879
Nuno <nuno.oliveira@eucinovacaoportugal.com>
2026-07-24 17:40:50 +0100

    new ex

--- If.java
+++ If.java
@@ -13,5 +13,23 @@ public class If{
         }else{
             System.out.println("It's not rainning");
         }
+
+        //Short hand if... else
+        //Syntaxe variable = (condition) ? expressionTrue :  expressionFalse;
+
+        String result = (rain) ? "Ternary expression if true" : "Ternary expression if false";
+        System.out.println(result);
+
+        //this method uses a variable to store the result, it can be printed directely intead of storing
+
+        System.out.println((rain) ? "Another ternary expression if true" : "Another ternary expression if false");
+
+        //Nested Ternary
+
+        int time = 22;
+        String resultTime = (time < 12) ? "Good morning." : 
+        (time < 18) ? "Good afternoon." : 
+        "Good evening.";
+        System.out.println(resultTime);
     }
 }
